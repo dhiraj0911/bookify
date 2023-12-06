b94e4c701d8a4e7b01ce81faf3cd542bf9fb8a56
dhiraj0911 <djirajbari123@gmail.com>
2023-12-07 02:25:00 +0530

    fix user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
-const User = require('../models/User')
+const User = require('../models/user')
 const {sendResponseError} = require('../middleware/middleware')
 const {checkPassword, newToken} = require('../utils/utility.function')
 
--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -1,4 +1,4 @@
-const User = require('../models/User')
+const User = require('../models/user')
 const {verifyToken} = require('../utils/utility.function')
 
 const sendResponseError = (statusCode, msg, res) => {
